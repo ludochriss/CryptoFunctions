9a39d1011784490ba04ba4c3bfac03b2790769e6
ludochris <ludochriss@gmail.com>
2024-04-23 17:49:47 +1000

    created trading view alert orders

---
+++ CreateTradingViewAlertOrder.cs
@@ -0,0 +1,68 @@
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Azure.Data.Tables;
+using Binance.Spot.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Services;
+using Services.Functions;
+
+namespace CryptoFunctions
+{
+    public class CreateTradingViewAlertOrder : BaseFunction<CreateTradingViewAlertOrder>
+    {
+
+        private readonly CryptoService _cryptoService;
+        public CreateTradingViewAlertOrder(ILogger<CreateTradingViewAlertOrder> logger, TableService tableService, RestApiService apiService, CryptoService cryptoService) : base(logger, tableService, apiService)
+        {
+            _cryptoService = cryptoService;
+        }
+
+        [Function("CreateTradingViewAlertOrder")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            string name = "CreateTradingViewAlertOrder";
+            JsonObject jResponse = new JsonObject();
+            try
+            {
+                logger.LogInformation($"{name}: Started");
+                var jBody = await JsonSerializer.DeserializeAsync<JsonObject>(req.Body);
+                string orderBindingId = Guid.NewGuid().ToString();
+                string symbol = jBody["symbol"]?.ToString() ?? string.Empty;
+                if (string.IsNullOrEmpty(symbol)) throw new Exception("Symbol not found");
+                decimal quantity = decimal.Parse(jBody["quantity"]?.ToString() ?? "0");
+                //TODO: check the amount in the account before submitting the order to make sure this can actually be done
+                if (quantity == 0) throw new Exception("Quantity not valid");
+                var sideValue = jBody["side"]?.ToString() ?? string.Empty;
+                if (string.IsNullOrEmpty(sideValue) ||
+                    (sideValue.ToLower() != "buy" && sideValue.ToLower() != "sell"))
+                         throw new Exception("Side not valid");
+                var side = sideValue.ToLower() == "buy" ? Side.BUY : Side.SELL;
+                TableEntity te = new TableEntity
+                {
+                    PartitionKey = symbol,
+                    RowKey = orderBindingId
+                };
+                te.Add("executed", false);
+                te.Add("quantity", quantity);
+                te.Add("side", side.ToString());
+                var tResult = await _tableService.UpsertAsync("TradingViewAlertOrders", te);
+                jResponse.Add("Successful", tResult);
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+                logger.LogError($"{name}: Exception: {ex.Message}");
+                jResponse.Add("error", ex.Message); 
+                jResponse.Add("exceptionType", ex.GetType().ToString());
+            }
+            finally
+            {
+                logger.LogInformation($"{name}: Complete");
+            }
+            return await _restApiService.HandleHttpResponseAsync(req, HttpStatusCode.OK, jResponse);
+        }
+    }
+}
--- TradingViewAlertTrigger.cs
+++ TradingViewAlertTrigger.cs
@@ -1,9 +1,9 @@
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using Azure.Data.Tables;
+using Binance.Common;
 using Binance.Spot.Models;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Services;
@@ -22,78 +22,129 @@ namespace CryptoFunctions
         }
 
         [Function("TradingViewAlertTrigger")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
-        {            
-                string name = "TradingViewAlertTrigger";
-                logger.LogInformation($"{name}: Started");
-                string result  =  string.Empty;
-                JsonObject jsonResponse = new JsonObject();
-                try
+        public async Task Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+        {
+            string name = "TradingViewAlertTrigger";
+            logger.LogInformation($"{name}: Started");
+            string result = string.Empty;
+            JsonObject jsonResponse = new JsonObject();
+            string tableName = "TradingViewAlertOrders";
+            try
+            {
+                //types of requests
+                //1. Execute pre-determined orders from table storage, spot stop loss and take profit
+                //2. Execute order with details from the trading view body can use as stop loss on trend line break or entry
+                //3. Create trading strategy rules in table storage and have the strategy execute from the alert, query table storage and execute the strategy
+                //4. Execute an order from an id in the alert body
+                if (req.Body == null)
                 {
-                    //types of requests
-                    //1. Execute pre-determined orders from table storage, spot stop loss and take profit
-                    //2. Execute order with details from the trading view body can use as stop loss on trend line break or entry
-                    //3. Create trading strategy rules in table storage and have the strategy execute from the alert, query table storage and execute the strategy
-                    if (req.Body == null)
-                    {
-                        logger.LogError($"{name}: No body in the alert request");
-                        return new BadRequestObjectResult(new {message = "Please pass body in the alert request body"});
-                        //TODO: need to alert me/user that the alert failed somehow
-                    }
-                    JsonObject requestBody =req.Body != null? await JsonSerializer.DeserializeAsync<JsonObject>(req.Body)?? new JsonObject(): new JsonObject();
-                    var side = requestBody.FirstOrDefault(x => x.Key == "side").Value.ToString().ToLower()  == "buy" ? Side.BUY : Side.SELL;
-                    var symbol = requestBody.FirstOrDefault(x => x.Key == "symbol").Value.ToString();
-                    var price = requestBody.FirstOrDefault(x => x.Key == "price").Value.ToString();
-                    var quantity = requestBody.FirstOrDefault(x => x.Key == "quantity").Value.ToString();
-                    var orderId = requestBody.FirstOrDefault(x => x.Key == "orderId").Value.ToString();
-                    //logger.LogInformation($"{name}:  {symbol} {price} {quantity} {orderId} extracted from body");
-                    if (string.IsNullOrEmpty(side.ToString()) || string.IsNullOrEmpty(symbol) || string.IsNullOrEmpty(price) || string.IsNullOrEmpty(quantity) || string.IsNullOrEmpty(orderId))
-                    {
-                        logger.LogError($"{name}: Missing required parameters in the alert request");
-                        return new BadRequestObjectResult(new {message= "Please pass action, symbol, price, quantity and orderId in the alert request body"});
-                    }
+                    logger.LogError($"{name}: No body in the alert request");
+                    return;
+                    //TODO: need to alert me/user that the alert failed somehow
+                }
+
+                JsonObject requestBody = req.Body != null ? await JsonSerializer.DeserializeAsync<JsonObject>(req.Body) ?? new JsonObject() : new JsonObject();
+
+                string id = requestBody["triggerId"]?.ToString() ?? string.Empty;
+                if (string.IsNullOrWhiteSpace(id)) logger.LogError($"{name} : No triggerId in the alert request");
+
+                string filter = $"RowKey eq '{id}' and executed eq false";
+                var tResult = await _tableService.QueryAsync(filter, tableName);
+                if (tResult.Count == 0) 
+                {
+                    logger.LogError($"{name} : No order found with id {id}");   
+                    return;                  
+                }
+                TableEntity te  = tResult.First();
+                string symbol = te["PartitionKey"]?.ToString()?? string.Empty;
+                if (string.IsNullOrWhiteSpace(symbol)) {
+                    logger.LogError($"{name} : No symbol found in order with id {id}");
+                    return;
+                }
+                decimal quantity = decimal.Parse(te["quantity"]?.ToString()?? "0");
+                if(quantity == 0)
+                {
+                    logger.LogError($"{name} : No quantity found in order with id {id}");
+                    return;
+                }
+                string sideValue = te["side"]?.ToString()?? string.Empty;
+                if(string.IsNullOrWhiteSpace(sideValue) || (sideValue.ToLower() != "buy" && sideValue.ToLower() != "sell"))
+                {
+                    logger.LogError($"{name} : No valid side found in order with id {id}");
+                    return;
+                }
+                Side side = sideValue.ToLower() == "buy" ? Side.BUY : Side.SELL;
+
+                var bResult  = await  _cryptoService.SpotAccountTrade.NewOrder(symbol, side, OrderType.MARKET,null, quantity);
+                logger.LogInformation($"{name}: Order Executed: \n{bResult}");
+                JsonDocument jResult = JsonDocument.Parse(bResult);
+                jResult.RootElement.TryGetProperty("status", out JsonElement status);
+                bool orderFilled = status.ToString().ToLower() == "filled"? true:false;
+                if(orderFilled){
+                    te["executed"] = true;
+                    await _tableService.UpsertAsync(tableName, te);
+                }
+                else{
+                    logger.LogError($"{name}: Order not filled: \n{bResult}");
+                }
+        
                 
-                    decimal dquantity = decimal.Parse(quantity);    
-                    decimal dprice = decimal.Parse(price);  
-                    logger.LogInformation($"Quantity and price parsed.");
+                
 
 
-                    logger.LogInformation($"Attempting to execute order");
-                    result = await _cryptoService.SpotAccountTrade.NewOrder(symbol, side,OrderType.LIMIT,TimeInForce.GTC, dquantity,null,dprice );
-                    logger.LogInformation($"Order Executed: \n{result}");
-                    jsonResponse.Add("orderResult", result);
-                    // TradingViewAlertModel tv = new TradingViewAlertModel(action, symbol, decimal.Parse(price), decimal.Parse(quantity), orderId);
-                    TableEntity te = new TableEntity();
-                    JsonDocument response =JsonDocument.Parse(result);
+                //TODO: remove if not used - 23/04this is for an order in the body, probably not going to use but keep for now 
+                // var side = requestBody.FirstOrDefault(x => x.Key == "side").Value.ToString().ToLower()  == "buy" ? Side.BUY : Side.SELL;
+                // var symbol = requestBody.FirstOrDefault(x => x.Key == "symbol").Value.ToString();
+                // var price = requestBody.FirstOrDefault(x => x.Key == "price").Value.ToString();
+                // var quantity = requestBody.FirstOrDefault(x => x.Key == "quantity").Value.ToString();
+                // var orderId = requestBody.FirstOrDefault(x => x.Key == "orderId").Value.ToString();
+                // //logger.LogInformation($"{name}:  {symbol} {price} {quantity} {orderId} extracted from body");
+                // if (string.IsNullOrEmpty(side.ToString()) || string.IsNullOrEmpty(symbol) || string.IsNullOrEmpty(price) || string.IsNullOrEmpty(quantity) || string.IsNullOrEmpty(orderId))
+                // {
+                //     logger.LogError($"{name}: Missing required parameters in the alert request");
+                //     return new BadRequestObjectResult(new {message= "Please pass action, symbol, price, quantity and orderId in the alert request body"});
+                // }
 
-                    foreach (var property in response.RootElement.EnumerateObject())
-                    {
-                        string propertyName = property.Name;
-                        object propertyValue = property.Value;
-                        te[property.Name] = propertyValue.ToString();
-                    }
-                    te.Add("PartitionKey", $"TradingViewAlert-{symbol}");
-                    te.Add("RowKey", _cryptoService.GenerateNewBinanceOrderId().ToString());
-                    logger.LogInformation($"Response parsed. Attempting to upsert to table storage.");
-                    //TODO: change this to a specific table for confirmations
-                    var tResult = await _tableService.UpsertAsync(te);
-                    logger.LogInformation($"{name}: {result}");
-                    jsonResponse.Add("upsertResult", tResult);
+                // decimal dquantity = decimal.Parse(quantity);    
+                // decimal dprice = decimal.Parse(price);  
+                // logger.LogInformation($"Quantity and price parsed.");
 
-                    
 
-                }
-                catch (Exception ex)
-                {
-                    // Handle exception
-                    logger.LogError($"{name}: Exception: {ex.Message}");
-                }
-                finally
-                {
-                    logger.LogInformation($"{name}: Complete");
-                }           
+                // logger.LogInformation($"Attempting to execute order");
+                // result = await _cryptoService.SpotAccountTrade.NewOrder(symbol, side,OrderType.LIMIT,TimeInForce.GTC, dquantity,null,dprice );
+                // logger.LogInformation($"Order Executed: \n{result}");
+                // jsonResponse.Add("orderResult", result);
+                // // TradingViewAlertModel tv = new TradingViewAlertModel(action, symbol, decimal.Parse(price), decimal.Parse(quantity), orderId);
+                // TableEntity te = new TableEntity();
+                // JsonDocument response =JsonDocument.Parse(result);
 
-                return new OkObjectResult(jsonResponse);
+                // foreach (var property in response.RootElement.EnumerateObject())
+                // {
+                //     string propertyName = property.Name;
+                //     object propertyValue = property.Value;
+                //     te[property.Name] = propertyValue.ToString();
+                // }
+                // te.Add("PartitionKey", $"TradingViewAlert-{symbol}");
+                // te.Add("RowKey", _cryptoService.GenerateNewBinanceOrderId().ToString());
+                // logger.LogInformation($"Response parsed. Attempting to upsert to table storage.");
+                // //TODO: change this to a specific table for confirmations
+                // var tResult = await _tableService.UpsertAsync(te);
+                // logger.LogInformation($"{name}: {result}");
+                // jsonResponse.Add("upsertResult", tResult);
+            }
+            catch(BinanceHttpException ex){
+                logger.LogError($"{name}: BinanceHttpException: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+                logger.LogError($"{name}: Exception: {ex.Message}");
+            }
+            finally
+            {
+                logger.LogInformation($"{name}: Complete");
+            }
+
         }
     }
 }
